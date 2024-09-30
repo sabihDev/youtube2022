c130b73308cbf3d4317e88a05bf30288d3079224
sabihDev <8203sabi.rahman@gmail.com>
2024-09-30 16:34:07 +0500

    contact component created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts"
 import "./Home.scss"
 import Categories from '../../components/Categories/Categories'
+import Contact from "../../components/Contact/Contact"
+
 const Home = () => {
   return (
     <div className='home'>
@@ -10,6 +12,7 @@ const Home = () => {
       <FeaturedProducts type="featured"/>
       <Categories/>
       <FeaturedProducts type="trending"/>
+      <Contact />
     </div>
   )
 }
